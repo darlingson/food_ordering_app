8f0caab043d874ffc6fb953e74d95ebbfa61005c
darlingson <dmakuwila@gmail.com>
2024-06-01 22:38:43 +0200

    added styling to the cards

--- app/src/main/java/com/codeshinobi/foodorderingapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/codeshinobi/foodorderingapp/ui/screens/HomeScreen.kt
@@ -3,6 +3,8 @@ package com.codeshinobi.foodorderingapp.ui.screens
 import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -10,6 +12,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
@@ -20,7 +23,10 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.codeshinobi.foodorderingapp.services.MenuItemService
 import io.appwrite.models.Document
@@ -37,6 +43,7 @@ fun HomeScreen(
     val numbers = (0..20).toList()
     var menuitems by remember { mutableStateOf<List<Document<Map<String, Any>>>>(listOf()) }
     val coroutineScope = rememberCoroutineScope()
+    var search by remember { mutableStateOf("") }
     LaunchedEffect(menuItemsService) {
         coroutineScope.launch {
             menuitems = menuItemsService.fetch()
@@ -47,24 +54,41 @@ fun HomeScreen(
         Log.d(user.toString(), user.value!!.email)
         Column {
             Text(text = "Hello $name")
+            TextField(
+                value =search ,
+                onValueChange = { search = it },
+                label = { Text(text = "Search") },
+                modifier = Modifier.fillMaxWidth().padding(10.dp)
+            )
             LazyVerticalGrid(
-                columns = GridCells.Adaptive(minSize = 200.dp),
+                columns = GridCells.Adaptive(minSize = 150.dp),
                 modifier = Modifier.fillMaxSize()
             ) {
                 items(menuitems.size) {
                     Column(horizontalAlignment = Alignment.CenterHorizontally) {
                         Card(
-                            modifier = Modifier.size(width = 150.dp, height = 100.dp),
+                            modifier = Modifier.size(
+                                width = 150.dp,
+                                height = 150.dp)
+                                .padding(10.dp),
                             shape = CardDefaults.elevatedShape
                         ) {
                             menuitems[it].data["title"]?.toString()?.let {
-                                Text(text = it)
+                                Text(text = it,
+                                    style = TextStyle(
+                                        fontWeight = FontWeight.Bold
+                                    )
+                                )
                             }
                             menuitems[it].data["description"]?.toString()?.let {
-                                Text(text = it)
+                                Text(text = it,maxLines = 4)
                             }
                             menuitems[it].data["price"]?.toString()?.let {
-                                Text(text = it)
+                                Text(text = "K $it",
+                                    style = TextStyle(
+                                        fontWeight = FontWeight.SemiBold
+                                    )
+                                )
                             }
                         }
                     }
